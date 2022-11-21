218cd653f231bbb997b88bc99e108ad858d96130
jason@decisions.com <jason@decisions.com>
2022-11-21 13:07:39 -0500

    [DT-035414] Override key fields for Experian Verify Request

--- Decisions.ExperianServices/Dao/Verify/ExperianVerifyRequest.cs
+++ Decisions.ExperianServices/Dao/Verify/ExperianVerifyRequest.cs
@@ -9,94 +9,123 @@ namespace Decisions.ExperianServices.Dao.Verify
     [Writable]
     public class ExperianVerifyRequest : ExperianCreditReportRequest
     {
-        [DataContract]
-        [Writable]
-        public new class ConsumerPii
-        {
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("applicantInformation")]
-            public ApplicantInformation ApplicantInformation { get; set; }
-        }
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("consumerPii")]
+        public new ConsumerPii ConsumerPii { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("requestor")]
+        public new Requestor Requestor { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("permissiblePurpose")]
+        public new PermissiblePurpose PermissiblePurpose { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("resellerInfo")]
+        public new ResellerInfo ResellerInfo { get; set; }
         
-        [DataContract]
-        [Writable]
-        public class ApplicantInformation
-        {
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("name")]
-            public Name Name { get; set; }
+        [JsonProperty("reportType")]
+        public string ReportType { get; set; }
 
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("ssn")]
-            public Ssn Ssn { get; set; }
+        [JsonProperty("vendorData")]
+        public new VendorData VendorData { get; set; }
 
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("dob")]
-            public Dob Dob { get; set; }
+        [JsonProperty("customOptions")]
+        public new CustomOptions CustomOptions { get; set; }
+    }
+    
+    [DataContract]
+    [Writable]
+    public class ConsumerPii
+    {
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("applicantInformation")]
+        public ApplicantInformation ApplicantInformation { get; set; }
+    }
+    
+    [DataContract]
+    [Writable]
+    public class ApplicantInformation
+    {
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("name")]
+        public Name Name { get; set; }
 
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("phone")]
-            public Phone[] Phone { get; set; }
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("ssn")]
+        public Ssn Ssn { get; set; }
 
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("currentAddress")]
-            public CurrentAddress CurrentAddress { get; set; }
-        }
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("dob")]
+        public Dob Dob { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("phone")]
+        public Phone[] Phone { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("currentAddress")]
+        public CurrentAddress CurrentAddress { get; set; }
+    }
+    
+    [DataContract]
+    [Writable]
+    public class CurrentAddress
+    {
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("line1")]
+        public string Line1 { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("line2")]
+        public string Line2 { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("line3")]
+        public string Line3 { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("zipCode")]
+        public long ZipCode { get; set; }
+    }
+    
+    [DataContract]
+    [Writable]
+    public class Requestor
+    {
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("subscriberCode")]
+        public string SubscriberCode { get; set; }
         
-        [DataContract]
-        [Writable]
-        public class CurrentAddress
-        {
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("line1")]
-            public string Line1 { get; set; }
-
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("line2")]
-            public string Line2 { get; set; }
-
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("line3")]
-            public string Line3 { get; set; }
-
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("city")]
-            public string City { get; set; }
-
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("state")]
-            public string State { get; set; }
-
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("zipCode")]
-            public long ZipCode { get; set; }
-        }
-        
-        [DataContract]
-        [Writable]
-        public new class Requestor
-        {
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("subscriberCode")]
-            public string SubscriberCode { get; set; }
-            
-            [DataMember]
-            [WritableValue]
-            [JsonProperty("verifierName")]
-            public string VerifierName { get; set; }
-        }
+        [DataMember]
+        [WritableValue]
+        [JsonProperty("verifierName")]
+        public string VerifierName { get; set; }
     }
 }
