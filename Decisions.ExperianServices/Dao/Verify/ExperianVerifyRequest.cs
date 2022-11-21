ad86f90ec7565ba196008babd34915a36d90988d
jason@decisions.com <jason@decisions.com>
2022-11-21 12:56:02 -0500

    [DT-036414] Override key fields for Experian Verify Request.

--- Decisions.ExperianServices/Dao/Verify/ExperianVerifyRequest.cs
+++ Decisions.ExperianServices/Dao/Verify/ExperianVerifyRequest.cs
@@ -1,6 +1,7 @@
 using System.Runtime.Serialization;
 using Decisions.ExperianServices.Dao.CreditReport;
 using DecisionsFramework.Design.ConfigurationStorage.Attributes;
+using Newtonsoft.Json;
 
 namespace Decisions.ExperianServices.Dao.Verify
 {
@@ -8,9 +9,94 @@ namespace Decisions.ExperianServices.Dao.Verify
     [Writable]
     public class ExperianVerifyRequest : ExperianCreditReportRequest
     {
-        /*
-         * So far there are no known differences between this and the Credit Report Request schema but we want to
-         * separate them out in case there are differences and for naming purposes.
-         */
+        [DataContract]
+        [Writable]
+        public new class ConsumerPii
+        {
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("applicantInformation")]
+            public ApplicantInformation ApplicantInformation { get; set; }
+        }
+        
+        [DataContract]
+        [Writable]
+        public class ApplicantInformation
+        {
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("name")]
+            public Name Name { get; set; }
+
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("ssn")]
+            public Ssn Ssn { get; set; }
+
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("dob")]
+            public Dob Dob { get; set; }
+
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("phone")]
+            public Phone[] Phone { get; set; }
+
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("currentAddress")]
+            public CurrentAddress CurrentAddress { get; set; }
+        }
+        
+        [DataContract]
+        [Writable]
+        public class CurrentAddress
+        {
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("line1")]
+            public string Line1 { get; set; }
+
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("line2")]
+            public string Line2 { get; set; }
+
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("line3")]
+            public string Line3 { get; set; }
+
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("city")]
+            public string City { get; set; }
+
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("state")]
+            public string State { get; set; }
+
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("zipCode")]
+            public long ZipCode { get; set; }
+        }
+        
+        [DataContract]
+        [Writable]
+        public new class Requestor
+        {
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("subscriberCode")]
+            public string SubscriberCode { get; set; }
+            
+            [DataMember]
+            [WritableValue]
+            [JsonProperty("verifierName")]
+            public string VerifierName { get; set; }
+        }
     }
 }
